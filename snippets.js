e4bcf58e71eaa2cd02eeeaf50495d23a8805151d
Rudi Antoni <rudi@wswork.com.br>
2023-10-21 02:49:13 -0300

    added javascript snippets

---
+++ snippets.js
@@ -0,0 +1,9 @@
+/*
+Run with
+  $ node snippets
+Or
+  $ node snippets.js
+
+If you don't have node installed, create a generic (index.html) page and add this script in the <head> tag:
+  <script src="./snippets.js"></script>
+*/
