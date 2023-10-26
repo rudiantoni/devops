8d4e09aba91d2f609d05b101478d7c69da4745d3
Rudi Antoni <rudiantoni2013@gmail.com>
2023-10-26 18:19:55 -0300

    updated java snippets

--- snippets/java8/Snippets.java
+++ snippets/java8/Snippets.java
@@ -10,69 +10,78 @@
  */
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Snippets {
   public static void main(String[] args) {
     
-    System.out.println("##################################################");
-    System.out.println("# Primitive types - Default values when creating arrays with size");
-    System.out.println("##################################################");
-    System.out.println("Default value for byte is 0");
-    System.out.println("--------------------------------------------------");
-    System.out.println(Arrays.toString(new byte[1]));
+    /**
+     * java.util.Arrays
+     * https://docs.oracle.com/javase/8/docs/api/java/util/Arrays.html
+     * 
+     * java.util.Objects
+     * https://docs.oracle.com/javase/8/docs/api/java/util/Objects.html
+     */
 
-    System.out.println("\nDefault value for short is 0");
-    System.out.println("--------------------------------------------------");
-    System.out.println(Arrays.toString(new short[1]));
+    print("##################################################");
+    print("# Primitive types - Default values when creating arrays with size");
+    print("##################################################");
+    print("Default value for byte is 0");
+    print("--------------------------------------------------");
+    print(Arrays.toString(new byte[1]));
 
-    System.out.println("\nDefault value for int is 0");
-    System.out.println("--------------------------------------------------");
-    System.out.println(Arrays.toString(new int[1]));
+    print("\nDefault value for short is 0");
+    print("--------------------------------------------------");
+    print(Arrays.toString(new short[1]));
 
-    System.out.println("\nDefault value for long is 0L");
-    System.out.println("--------------------------------------------------");
-    System.out.println(Arrays.toString(new long[1]));
+    print("\nDefault value for int is 0");
+    print("--------------------------------------------------");
+    print(Arrays.toString(new int[1]));
+
+    print("\nDefault value for long is 0L");
+    print("--------------------------------------------------");
+    print(Arrays.toString(new long[1]));
     
-    System.out.println("\nDefault value for float is 0.0f");
-    System.out.println("--------------------------------------------------");
-    System.out.println(Arrays.toString(new float[1]));
+    print("\nDefault value for float is 0.0f");
+    print("--------------------------------------------------");
+    print(Arrays.toString(new float[1]));
     
-    System.out.println("\nDefault value for double is 0.0d");
-    System.out.println("--------------------------------------------------");
-    System.out.println(Arrays.toString(new double[1]));
+    print("\nDefault value for double is 0.0d");
+    print("--------------------------------------------------");
+    print(Arrays.toString(new double[1]));
    
-    System.out.println("\nDefault value for char is '\u0000', null character for unicode");
-    System.out.println("--------------------------------------------------");
-    System.out.println(Arrays.toString(new char[1]));
+    print("\nDefault value for char is '\u0000', null character for unicode");
+    print("--------------------------------------------------");
+    print(Arrays.toString(new char[1]));
     
-    System.out.println("\nDefault value for boolean is false");
-    System.out.println("--------------------------------------------------");
-    System.out.println(Arrays.toString(new boolean[1]));
+    print("\nDefault value for boolean is false");
+    print("--------------------------------------------------");
+    print(Arrays.toString(new boolean[1]));
     
-    System.out.println();
-    System.out.println("##################################################");
-    System.out.println("# Arrays - Different ways to create one");
-    System.out.println("##################################################");
-    System.out.println("Declare array using standard Java");
-    System.out.println("--------------------------------------------------");
+    print();
+    print("##################################################");
+    print("# Arrays - Different ways to create one");
+    print("##################################################");
+    print("Declare array using standard Java");
+    print("--------------------------------------------------");
     int[] arr;
     
-    System.out.println("\nDeclare array using C-way (NOT RECOMMENDED)");
-    System.out.println("--------------------------------------------------");
+    print("\nDeclare array using C-way (NOT RECOMMENDED)");
+    print("--------------------------------------------------");
     int arr2[];
 
-    System.out.println("\nInitialize size of an declared array");
-    System.out.println("--------------------------------------------------");
+    print("\nInitialize size of an declared array");
+    print("--------------------------------------------------");
     int arr3[];
     arr3 = new int[10];
 
-    System.out.println("\nDeclare and initialize array size inline");
-    System.out.println("--------------------------------------------------");
+    print("\nDeclare and initialize array size inline");
+    print("--------------------------------------------------");
     int arr4[] = new int[10];
 
 
-    System.out.println("\nAssigning values to each array position");
-    System.out.println("--------------------------------------------------");
+    print("\nAssigning values to each array position");
+    print("--------------------------------------------------");
     arr4[0] = 262; // First element in every array
     arr4[1] = 860;
     arr4[2] = 56;
@@ -84,28 +93,66 @@ public class Snippets {
     arr4[8] = 181;
     arr4[9] = -65; // Last element in a 10 elements array
 
-    System.out.println("\nArray literals: Declare, initialize and assign values to an array and each of its elements");
-    System.out.println("--------------------------------------------------");
+    print("\nArray literals: Declare, initialize and assign values to an array and each of its elements");
+    print("--------------------------------------------------");
     int[] arr5 = {23, 1224, 124, -50, 154, -65, 26, -87, -912, 45};
 
-    System.out.println("\nObtaining array size");
-    System.out.println("--------------------------------------------------");
-    System.out.println("arr5.length = " + arr5.length);
+    print("\nObtaining array size");
+    print("--------------------------------------------------");
+    print("arr5.length = " + arr5.length);
    
-    System.out.println("\nAcessing array first element");
-    System.out.println("--------------------------------------------------");
-    System.out.println("arr5[0] = " + arr5[0]);
+    print("\nAcessing array first element");
+    print("--------------------------------------------------");
+    print("arr5[0] = " + arr5[0]);
 
-    System.out.println("\nAcessing array last element");
-    System.out.println("--------------------------------------------------");
-    System.out.println("arr5[arr5.length - 1] = " + arr5[arr5.length - 1]);
+    print("\nAcessing array last element");
+    print("--------------------------------------------------");
+    print("arr5[arr5.length - 1] = " + arr5[arr5.length - 1]);
     
-    // Throws ArrayIndexOutOfBoundsException exception
-    // System.out.println("\nAcessing array unexistent element ")
-    // System.out.println("--------------------------------------------------")
-    // System.out.println("arr5[10] = " + arr5[10]);
+    // Throws ArrayIndexOutOfBoundsException
+    // print("\nAcessing array unexistent element ")
+    // print("--------------------------------------------------")
+    // print("arr5[10] = " + arr5[10]);
     
+    print();
+    print("##################################################");
+    print("# Null Checking");
+    print("##################################################");
+    String objCheckNull = null;
+    String objCheckNotNull = "This is a non null String object.";
 
+    print("\nUsing primitive checking");
+    print("--------------------------------------------------");
+    print(objCheckNull == null);
+    print(objCheckNull != null);
+    print(objCheckNotNull == null);
+    print(objCheckNotNull != null);
+    
+    print("\nUsing java.util.Objects");
+    print("--------------------------------------------------");
+        
+    print("\nUsing Objects.nonNull(Object o)");
+    print(Objects.nonNull(objCheckNull));
+    print(Objects.nonNull(objCheckNotNull));
+    // Returns true if object is not null, false otherwise
 
+    print("\nUsing Objects.isNull(Object o)");
+    print(Objects.isNull(objCheckNull));
+    print(Objects.isNull(objCheckNotNull));
+    // Returns true if object is null, false otherwise
+
+    print("\nUsing Objects.requireNonNull(Object o)");
+    // print(Objects.requireNonNull(objCheckNull)); // Throws java.lang.NullPointerException
+    print(Objects.requireNonNull(objCheckNotNull)); 
+    // Returns the object if is not null, throw exception otherwise
+   
   }
+
+  private static void print(Object o) {
+    System.out.println(o);
+  }
+  private static void print() {
+    System.out.println();
+  }
+
 }
