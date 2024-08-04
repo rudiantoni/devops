4186ec4f31cd7ed06664f7e92a27fc6ff12831be
Rudi Antoni <rudiantoni2013@gmail.com>
2024-08-04 01:14:06 -0300

    added mysql

---
+++ snippets/queries_mysql.sql
@@ -0,0 +1,15 @@
+-- ##################################################
+-- References
+--
+-- MySQL: https://www.postgresql.org/docs/14/index.html
+--
+-- Below queries are supported (and tested) in MySQL version 8
+--
+-- ##################################################
+
+-- ##################################################
+-- # Using native related functionality
+-- ##################################################
+-- Get database version
+-- --------------------------------------------------
+SELECT VERSION();
