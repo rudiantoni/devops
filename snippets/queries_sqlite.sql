2eaef3fc8c4db4d90509accb1e10db644da53e77
Rudi Antoni <rudiantoni2013@gmail.com>
2024-08-04 01:11:08 -0300

    added sqlite

---
+++ snippets/queries_sqlite.sql
@@ -0,0 +1,15 @@
+-- ##################################################
+-- References
+--
+-- SQLite: https://www.sqlite.org/docs.html
+--
+-- Below queries are supported (and tested) in SQLite version 3
+--
+-- ##################################################
+
+-- ##################################################
+-- # Using native related functionality
+-- ##################################################
+-- Get database version
+-- --------------------------------------------------
+SELECT SQLITE_VERSION();
