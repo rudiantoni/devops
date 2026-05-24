4e23a267cedfbd22fd869966104bc9bf79dd9bfc
Rudi Antoni <rudiantoni2013@gmail.com>
2026-05-24 16:39:48 -0300

    updated script java snippets

---
+++ snippets/java8/run_snippets.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Using Windows Command Prompt or Windows PowerShell
+REM cd to this directory (snippets\java8)
+REM run_snippets.bat
+
+javac *.java
+
+java Snippets
+
+del *.class
--- snippets/java8/run_snippets.sh
+++ snippets/java8/run_snippets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Using Git Bash or Ubuntu terminal
+# Using Windows Git Bash or Ubuntu terminal
 # cd to this directory (snippets/java8)
 # sh run_snippets.sh
 
